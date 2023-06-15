140e698c31f5594f1b1d6fefb95eca5701ecdd5f
Luan Trajano <luan.trajano@biz.com.br>
2023-06-15 20:42:14 -0300

    feat: get categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,9 +9,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (resquest, response) => {
   const { name, description } = resquest.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.listen();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
