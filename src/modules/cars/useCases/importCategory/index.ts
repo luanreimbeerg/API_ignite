7341520fb411798343dcf3c3dc23752787d6f48d
Luan Trajano <luan.trajano@biz.com.br>
2023-06-16 01:06:52 -0300

    feat: add multer, csv-parse and route import

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,10 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUsecase } from "./ImportCategoryUseCase";
+
+const importCategoryUseCase = new ImportCategoryUsecase();
+
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
+
+export { importCategoryController };
